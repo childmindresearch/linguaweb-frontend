6c05ae2fa9d5af671286a79ef538f4db23abd7a4
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-01-24 13:32:00 -0500

    Fix light mode detection in static builds (#64)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
+	import NavBar from '$lib/components/NavBar.svelte';
+	import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom';
+	import { AppShell, autoModeWatcher, storePopup } from '@skeletonlabs/skeleton';
+	import { onMount } from 'svelte';
 	import '../app.postcss';
 
-	import { AppShell } from '@skeletonlabs/skeleton';
-	import NavBar from '$lib/components/NavBar.svelte';
-
-	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
-	import { storePopup } from '@skeletonlabs/skeleton';
+	onMount(() => {
+		autoModeWatcher();
+	});
 
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
